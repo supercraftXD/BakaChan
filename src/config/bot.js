2e0797ebac0e21c460a79c8d3865bf9d890c1eea
supercraftXD <60427968+supercraftXD@users.noreply.github.com>
2026-09-15 20:31:41 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "I'm BakaChan made by: Evlian :'3",     // this is what people actually see
+        state: "Waiting.. ur Commands :'3",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
