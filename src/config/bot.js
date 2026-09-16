5e566d139b12c7c2fd5e35d354c3065ed37311b6
supercraftXD <60427968+supercraftXD@users.noreply.github.com>
2026-09-16 13:28:14 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "red",
+      secondary: "red",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
